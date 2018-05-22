b91653f1d498f7b9513d185abe042715db74f020
fszhouzz <fszhouzz@qq.com>
2018-05-22 11:42:56 +0800

    天、時辰干支計算

--- Cj2356InputMethod2/src/com/zzz/cj2356inputMethod/utils/DateUtils.java
+++ Cj2356InputMethod2/src/com/zzz/cj2356inputMethod/utils/DateUtils.java
@@ -11,17 +11,14 @@ import com.zzz.cj2356inputMethod.dto.Item;
 
 public class DateUtils {
 
-    private static final String[] shiShens = { "子", "丑", "丑", "寅", "寅", "卯",
-            "卯", "辰", "辰", "巳", "巳", "午", "午", "未", "未", "申", "申", "酉", "酉",
-            "戌", "戌", "亥", "亥", "子" };
-    private static final String[] kes = { "初刻", "二刻", "三刻", "四刻", "五刻", "六刻",
-            "七刻", "末刻" };
+    private static final String[] shiShens = { "子", "丑", "丑", "寅", "寅", "卯", "卯", "辰", "辰", "巳", "巳", "午", "午", "未",
+            "未", "申", "申", "酉", "酉", "戌", "戌", "亥", "亥", "子" };
+    private static final String[] kes = { "初刻", "二刻", "三刻", "四刻", "五刻", "六刻", "七刻", "末刻" };
 
     /** 格式化 */
     public static String formatDate(Date date, String format) {
         try {
-            SimpleDateFormat sdf = new SimpleDateFormat(format,
-                    Locale.TRADITIONAL_CHINESE);
+            SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.TRADITIONAL_CHINESE);
             return sdf.format(date);
         } catch (Exception e) {
         }
@@ -62,9 +59,19 @@ public class DateUtils {
             items.add(new Item(null, item.getGenCode(), null, formatDate(now, "EEEE").replace("星期", "周")));
         } else if ("時辰".equals(item.getCharacter()) || "时辰".equals(item.getCharacter())
                 || "辰".equals(item.getCharacter())) {
-            List<String> shis = getShiShen(new Date());
-            for (String str : shis) {
-                items.add(new Item(null, item.getGenCode(), null, str));
+            try {
+                Date now = new Date();
+                List<String> shis = new ArrayList<String>();
+                String hourGz = DateGanzhiTest.getHourGanzhi(now);
+                String quarterTimeStr = DateGanzhiTest.getQuarterTimeStr(now);
+                shis.add(hourGz + "時");
+                shis.add(hourGz + "时");
+                shis.add(hourGz + "時" + quarterTimeStr);
+                shis.add(hourGz + "时" + quarterTimeStr);
+                for (String str : shis) {
+                    items.add(new Item(null, item.getGenCode(), null, str));
+                }
+            } catch (Exception e) {
             }
         }
         return items;
@@ -90,52 +97,24 @@ public class DateUtils {
             String chineseDate = HialiUtils.getChineseCalByWest(now);
             String ganzhi = HialiUtils.getGanZhiByChinesYear(
                     Integer.parseInt(HialiUtils.replaceChinaNumberByArab(chineseDate.split("年")[0].replace("前", "-"))));
+            String dateGanzhiStr = DateGanzhiTest.getDateGanzhi(now) + "日";
+            dateGanzhiStr += DateGanzhiTest.getHourGanzhi(now) + (isSimp ? "时" : "時");
+            dateGanzhiStr += DateGanzhiTest.getQuarterTimeStr(now);
             items.add(new Item(null, item.getGenCode(), null,
-                    formatDate(now, "yyyy年") + ganzhi + chineseDate.split("年")[1] + getShiShen(now).get(3)));
+                    formatDate(now, "yyyy年") + ganzhi + chineseDate.split("年")[1] + dateGanzhiStr));
             items.add(new Item(null, item.getGenCode(), null, (isSimp ? "夏历" : "夏曆") + chineseDate.split("年")[0] + "年"
-                    + ganzhi + chineseDate.split("年")[1] + getShiShen(now).get(3)));
+                    + ganzhi + chineseDate.split("年")[1] + dateGanzhiStr));
         } catch (Exception e) {
         }
         return items;
     }
 
-    /**
-     * 得到時辰
-     */
-    private static List<String> getShiShen(Date date) {
-        List<String> result = new ArrayList<String>();
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        int hour = cal.get(Calendar.HOUR_OF_DAY);
-        String shiShen = shiShens[hour];
-        result.add(shiShen + "時"); // 0某時
-        result.add(shiShen + "时"); // 1某时
-        String couZheng = (hour % 2 == 1) ? "初" : "正";
-        result.add(shiShen + couZheng); // 2某初，某正
-
-        int minute = cal.get(Calendar.MINUTE);
-        String ke1 = kes[minute / 15];
-        result.add(shiShen + couZheng + ke1); // 3某初某刻，某正某刻
-
-        // 4 某時初-末刻，某时初-末日刻
-        if ("初".equals(couZheng)) {
-            result.add(shiShen + "時" + ke1);
-            result.add(shiShen + "时" + ke1);
-        } else {
-            result.add(shiShen + "時" + kes[minute / 15 + 4]);
-            result.add(shiShen + "时" + kes[minute / 15 + 4]);
-        }
-        return result;
-    }
-
     /** 兩個日期相隔天數，日期不同就算一天 */
-    public static int datesMoreAfterBegin(Date begin, Date end)
-            throws Exception {
+    public static int datesMoreAfterBegin(Date begin, Date end) throws Exception {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
         Date beginDate = sdf.parse(sdf.format(begin)); // 格式yyyyMMdd
         Date endDate = sdf.parse(sdf.format(end));
-        return ((Long) ((endDate.getTime() - beginDate.getTime()) / (1000 * 60 * 60 * 24)))
-                .intValue();
+        return ((Long) ((endDate.getTime() - beginDate.getTime()) / (1000 * 60 * 60 * 24))).intValue();
     }
 
     /** 兩個日期相差天數，滿一天才算一天 */
@@ -143,8 +122,7 @@ public class DateUtils {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
         Date beginDate = sdf.parse(sdf.format(begin)); // 格式yyyyMMdd
         Date endDate = sdf.parse(sdf.format(end));
-        return ((Long) ((endDate.getTime() - beginDate.getTime()) / (1000 * 60 * 60 * 24)))
-                .intValue();
+        return ((Long) ((endDate.getTime() - beginDate.getTime()) / (1000 * 60 * 60 * 24))).intValue();
     }
 
 }
